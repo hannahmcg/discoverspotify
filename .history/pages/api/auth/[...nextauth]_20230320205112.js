7b694f2469ab4bdedfd3a6a51a51d24eec10d368
test <test@MacBook-Pro-2.attlocal.net>
2023-08-04 22:34:11 -0700

    change nextauth url to deployed link

--- .history/pages/api/auth/[...nextauth]_20230320205112.js
+++ .history/pages/api/auth/[...nextauth]_20230320205112.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 import spotifyApi, {LOGIN_URL} from "../../../lib/spotify";
- 
+
 // refresh token function 
 async function refreshAccessToken(token) {
     try {
