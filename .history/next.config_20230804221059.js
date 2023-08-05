3f2f9c457eea0b32169d02fe4a45bf952dbb2d8e
test <test@MacBook-Pro-2.attlocal.net>
2023-08-04 22:11:12 -0700

    add plugin to module exports in next config

--- .history/next.config_20230804221059.js
+++ .history/next.config_20230804221059.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  extends: [
+    'plugin:@next/next/recommended',
+  ],
   reactStrictMode: true,
   images: {
     domains: ["i.scdn.co"],
